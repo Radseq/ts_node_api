328bbf47678bbad2e991e99cfda2f432523ac8ff
Maciej Plonka <maciej.plonka@hotmail.com>
2022-11-14 20:19:23 +0900

    refactor: Simplify products module

---
+++ src/products/routes.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import {getAllProducts, getProductById} from "./product";
+
+function createProductRouter() {
+    return express.Router()
+        .get('', async(_, resp) => {
+            const products = await getAllProducts()
+            resp.status(200).json(products);
+        })
+        .get('/:productId', async (req, resp) =>{
+            const product = await getProductById(req.params.productId);
+            if(!product){
+                return resp.status(404).send({
+                    error: `Product ${req.params.productId} not found`,
+                });
+            }
+            return resp.status(200).json(product);
+        } )
+}
+
+export const productRouter = createProductRouter();
+
--- src/products/services/products.service.ts
+++
@@ -1,9 +0,0 @@
-import { getProductById, getProducts } from '../daos/products.dao';
-
-export const list = async (limit: number, page: number) => {
-    return getProducts();
-}
-
-export const readById = async (id: string) => {
-    return getProductById(id);
-}
