4e0dd7a622668392821c8554b4c8dbc32afc0921
radseq <radek.gom@gmail.com>
2022-11-16 21:51:57 +0100

    fix: database table product, delete unnecessary columns, added usabled

--- src/products/prismaProductSeed.ts
+++ src/products/prismaProductSeed.ts
@@ -11,7 +11,9 @@ const main = async () => {
             price: 1926,
             discountPrice: 1720,
             quantity: 5,
-            category1: "Bestseller",
+            scoreValue: 5,
+            installmentPrice: 350,
+            hasFreeShipping: true,
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae possimus sapiente, ducimus asperiores error eum ipsam quasi repellendus iste earum laboriosam nesciunt officiis atque rerum quos incidunt aspernatur velit voluptates. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium labore perferendis minus incidunt doloribus, eligendi repudiandae earum dignissimos est neque deserunt consequatur. Repudiandae autem maxime magnam eveniet! Atque, suscipit amet. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium labore perferendis minus incidunt doloribus, eligendi repudiandae earum dignissimos est neque deserunt consequatur. Repudiandae autem maxime magnam eveniet! Atque, suscipit amet."
         },
     });
