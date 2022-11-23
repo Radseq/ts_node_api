d1bc6324fdfdb493f6a18e11f23a2ff293d0e6c9
Radosław <radekgom@gmail.com>
2022-11-23 18:11:55 +0100

    Update src/categories/category.ts
    Co-authored-by: Maciej Płonka <maciej.plonka@hotmail.com>

--- src/categories/category.ts
+++ src/categories/category.ts
@@ -19,12 +19,9 @@ const getCategoriesRelatedToProducts = async () => {
 
 export const getCategoriesByNameRelatedToProducts = async (categoryNames: string[]) => {
     console.log('getCategoriesByNameRelatedToProducts');
-    const prismaCategoryNameConstains = new Array<PrismaCategoryNameConstains>;
-
-    //add values search strings to array
-    categoryNames.forEach(categoryName => {
-        prismaCategoryNameConstains.push({ name: { contains: categoryName } });
-    });
+    const prismaCategoryNameConstains = categoryNames.map(
+     categoryName => ({ name:  { contains: categoryName } })
+    );
 
     // find all categories of customers search, and this categories are related to products
     const categories = await prisma.categories.findMany({
