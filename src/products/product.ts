70b42ec1277eaccc91b8a411d751129e9379b19d
Radseq <radek.gom@gmail.com>
2022-12-27 21:16:11 +0100

    feat: bestseller endpoint to provide last most sell products from date

--- src/products/product.ts
+++ src/products/product.ts
@@ -1,3 +1,4 @@
+import { CONFIG } from "../config";
 import { prisma } from "../../prisma/prisma";
 
 export const getAllProducts = async () => {
@@ -12,4 +13,41 @@ export const getProductById = async (productId: number) => {
     })
 
     return product;
+}
+
+export const getAllBestsellerProducts = async () => {
+    const bestsellerMonthsPriorToToday = new Date();
+    bestsellerMonthsPriorToToday.setMonth(bestsellerMonthsPriorToToday.getMonth() - CONFIG.LAST_MONTHS_PRODUCTS_IN_ORDER);
+
+    const orderProducts = await prisma.orderProduct.findMany({
+        where: {
+            addDate: {
+                gt: bestsellerMonthsPriorToToday
+            }
+        }
+    });
+
+    const soldProductsIds = orderProducts.filter(orderProduct => orderProduct.productId !== null)
+        .map(orderProduct => orderProduct.id);
+
+    const lastSoldProducts = await prisma.product.findMany({
+        where: {
+            id: {
+                in: [...new Set(soldProductsIds)]
+            },
+        }
+    });
+
+    const prodIdWithSellCount = lastSoldProducts.map(product => [product.id, orderProducts
+        .filter(filterProduct => filterProduct.productId === product.id).length]);
+
+    const productsIds = prodIdWithSellCount.sort((a, b) => b[1] - a[1])
+        .slice(0, CONFIG.MAX_BESTSELLS_PRODUCTS).map(aaa => aaa[0]);
+
+    const result = lastSoldProducts.filter(product => {
+        if (productsIds.includes(product.id))
+            return product;
+    });
+
+    return result;
 }
--- src/products/routes.ts
+++ src/products/routes.ts
@@ -1,21 +1,25 @@
-import express from "express";
-import {getAllProducts, getProductById} from "./product";
+import express, { Request, Response } from "express";
+import { getAllBestsellerProducts, getAllProducts, getProductById } from "./product";
 
 function createProductRouter() {
     return express.Router()
-        .get('', async(_, resp) => {
+        .get('', async (_, resp: Response) => {
             const products = await getAllProducts()
             resp.status(200).json(products);
         })
-        .get('/:productId', async (req, resp) =>{
+        .get('/bestsellers', async (_, resp: Response) => {
+            const bestsellerProducts = await getAllBestsellerProducts();
+            resp.status(200).json(bestsellerProducts);
+        })
+        .get('/:productId', async (req: Request, resp: Response) => {
             const product = await getProductById(Number(req.params.productId));
-            if(!product){
+            if (!product) {
                 return resp.status(404).send({
                     error: `Product ${req.params.productId} not found`,
                 });
             }
             return resp.status(200).json(product);
-        } )
+        })
 }
 
 export const productRouter = createProductRouter();
