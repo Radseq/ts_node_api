9b0438ff67c5d35ded9a5a55cbd69148cf86f2d2
Radseq <radek.gom@gmail.com>
2023-01-01 12:13:27 +0100

    refactor: getAllBestsellerProducts function, into few smallers functions

--- src/products/product.ts
+++ src/products/product.ts
@@ -1,5 +1,7 @@
 import { CONFIG } from "../config";
 import { prisma } from "../../prisma/prisma";
+import { getOrderProductsByDate } from "../orderProducts/orderProducts";
+import { OrderProduct } from "@prisma/client";
 
 export const getAllProducts = async () => {
     const allProducts = await prisma.product.findMany();
@@ -15,29 +17,19 @@ export const getProductById = async (productId: number) => {
     return product;
 }
 
-export const getAllBestsellerProducts = async () => {
-    const bestsellerMonthsPriorToToday = new Date();
-    bestsellerMonthsPriorToToday.setMonth(bestsellerMonthsPriorToToday.getMonth() - CONFIG.LAST_MONTHS_PRODUCTS_IN_ORDER);
-
-    const orderProducts = await prisma.orderProduct.findMany({
-        where: {
-            addDate: {
-                gt: bestsellerMonthsPriorToToday
-            }
-        }
-    });
-
-    const soldProductsIds = orderProducts.filter(orderProduct => orderProduct.productId !== null)
-        .map(orderProduct => orderProduct.id);
-
-    const lastSoldProducts = await prisma.product.findMany({
+export const getProductsByIds = async (productIds: number[]) => {
+    const product = await prisma.product.findMany({
         where: {
             id: {
-                in: soldProductsIds
+                in: productIds
             },
         }
-    });
+    })
 
+    return product;
+}
+
+const getMapProductIdWithSoldCount = (orderProducts: OrderProduct[]) => {
     let mapProductIdWithSoldCount = new Map<number, number>();
 
     for (const product of orderProducts) {
@@ -45,14 +37,32 @@ export const getAllBestsellerProducts = async () => {
             orderProducts.filter(filterProduct => filterProduct.productId === product.id).length);
     }
 
+    return mapProductIdWithSoldCount;
+}
+
+const getMostSoldProductsByDate = async (date: Date) => {
+    const orderProducts = await getOrderProductsByDate(date);
+
     // todo really dont know what name put instead productKeyValue
-    const productKeyValue = new Map([...mapProductIdWithSoldCount.entries()].sort((a, b) => b[1] - a[1])
+    const productKeyValue = new Map([...getMapProductIdWithSoldCount(orderProducts).entries()].sort((a, b) => b[1] - a[1])
         .slice(0, CONFIG.MAX_BESTSELLS_PRODUCTS));
 
-    const result = lastSoldProducts.filter(product => {
+    const soldProductsIds = orderProducts.filter(orderProduct => orderProduct.productId !== null)
+        .map(orderProduct => orderProduct.id);
+
+    const products = await getProductsByIds(soldProductsIds);
+
+    const result = products.filter(product => {
         if (productKeyValue.has(product.id))
             return product;
     });
 
     return result;
+}
+
+export const getAllBestsellerProducts = async () => {
+    const bestsellerMonthsPriorToToday = new Date();
+    bestsellerMonthsPriorToToday.setMonth(bestsellerMonthsPriorToToday.getMonth() - CONFIG.LAST_MONTHS_PRODUCTS_IN_ORDER);
+
+    return getMostSoldProductsByDate(bestsellerMonthsPriorToToday);
 }
