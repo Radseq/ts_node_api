e4735f841bc6a9e46e3648fc3f614e044d6470c0
Radseq <radek.gom@gmail.com>
2023-01-19 10:55:55 +0100

    fix: remove console log

--- src/advertising/advertisement.ts
+++ src/advertising/advertisement.ts
@@ -2,10 +2,9 @@ import { CONFIG } from "../config";
 import { prisma } from "../../prisma/prisma";
 
 export const getAdvertising = async () => {
-
     const fromMonthsPriorToToday = new Date();
     fromMonthsPriorToToday.setMonth(fromMonthsPriorToToday.getMonth() - CONFIG.ADV_FROM_MONTHS_PRIOR_TO_TODAY);
-    console.log('now', new Date())
+
     const allAdvertising = await prisma.advertisement.findMany({
         select: {
             id: true,
