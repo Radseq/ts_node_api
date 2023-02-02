59ccfc91c095a770f3d1fe7a29a65215a6a24667
Radseq <radek.gom@gmail.com>
2023-02-02 20:02:47 +0100

    feat: add zoe as validator for command and product routes

--- src/comments/routes.ts
+++ src/comments/routes.ts
@@ -1,19 +1,44 @@
 import express, { Request, Response } from "express";
+import { z } from "zod";
 import { getProductComments } from "./comment";
 
 function createCommentRouter() {
     return express.Router()
         .get('/:productId/:pageIndex/:pageSize', async (req: Request, resp: Response) => {
-            const productId = Number(req.params.productId);
-            const pageIndex = Number(req.params.pageIndex);
-            const pageSize = Number(req.params.pageSize);
-            const comments = await getProductComments(productId, pageIndex, pageSize);
-            if (!comments) {
-                return resp.status(404).send({
-                    error: `Comemnts for product id ${req.params.productId} not found`,
+            try {
+                const reqParms = z.object({
+                    productId: z.number(),
+                    pageIndex: z.number(),
+                    pageSize: z.number(),
                 });
+
+                const validationResult = reqParms.parse({
+                    productId: req.params.productId,
+                    pageIndex: req.params.pageIndex,
+                    pageSize: req.params.pageSize
+                });
+
+                const comments = await getProductComments(validationResult.productId, validationResult.pageIndex, validationResult.pageSize);
+                if (!comments) {
+                    return resp.status(404).send({
+                        error: `Comemnts for product id ${req.params.productId} not found`,
+                    });
+                }
+                return resp.status(200).json(comments);
+            } catch (error) {
+                if (error instanceof z.ZodError) {
+                    let Errors: { code: string, message: string }[] = []
+                    for (let index = 0; index < error.issues.length; index++) {
+                        Errors.push({
+                            code: error.issues[index].code,
+                            message: error.issues[index].message
+                        });
+                    }
+                    return resp.status(404).send({
+                        error: Errors,
+                    });
+                }
             }
-            return resp.status(200).json(comments);
         })
 }
 
--- src/products/routes.ts
+++ src/products/routes.ts
@@ -1,21 +1,43 @@
-import express from "express";
-import {getAllProducts, getProductById} from "./product";
+import express, { Request, Response } from "express";
+import { z } from "zod";
+import { getAllProducts, getProductById } from "./product";
 
-function createProductRouter() {
+export function createProductRouter() {
     return express.Router()
-        .get('', async(_, resp) => {
+        .get('', async (_, resp) => {
             const products = await getAllProducts()
             resp.status(200).json(products);
         })
-        .get('/:productId', async (req, resp) =>{
-            const product = await getProductById(Number(req.params.productId));
-            if(!product){
-                return resp.status(404).send({
-                    error: `Product ${req.params.productId} not found`,
-                });
+        .get('/:productId', async (req: Request, resp: Response) => {
+            try {
+                const productId = z.coerce.number({
+                    required_error: "productId is required",
+                    invalid_type_error: "productId must be a number",
+                }).min(0).parse(req.params.productId);
+
+                const product = await getProductById(productId);
+
+                if (!product) {
+                    return resp.status(404).send({
+                        error: `Product ${req.params.productId} not found`,
+                    });
+                }
+                return resp.status(200).json(product);
+            } catch (error) {
+                if (error instanceof z.ZodError) {
+                    let Errors: { code: string, message: string }[] = []
+                    for (let index = 0; index < error.issues.length; index++) {
+                        Errors.push({
+                            code: error.issues[index].code,
+                            message: req.params.productId + ": " + error.issues[index].message
+                        });
+                    }
+                    return resp.status(404).send({
+                        error: Errors,
+                    });
+                }
             }
-            return resp.status(200).json(product);
-        } )
+        })
 }
 
 export const productRouter = createProductRouter();
