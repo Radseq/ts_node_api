8fc4792391ba824935967a4c5e98ccc83d3a2ba6
Radseq <radek.gom@gmail.com>
2023-02-16 11:31:37 +0100

    fix: add comments, format code

--- src/products/product.ts
+++ src/products/product.ts
@@ -1,50 +1,67 @@
 import { CONFIG } from "../config";
 import { prisma } from "../../prisma/prisma";
 import { getOrderProductsByDate } from "../orderProducts/orderProducts";
-import { OrderProduct } from "@prisma/client";
 
 export const getAllProducts = async () => {
     const allProducts = await prisma.product.findMany();
 
     return allProducts;
-}
+};
 
 export const getProductById = async (productId: number) => {
     const product = await prisma.product.findUnique({
-        where: { id: productId }
-    })
+        where: { id: productId },
+    });
 
     return product;
-}
+};
 
 export const getProductsByIds = async (productIds: number[]) => {
     const product = await prisma.product.findMany({
         where: {
             id: {
-                in: productIds
+                in: productIds,
             },
-        }
-    })
+        },
+    });
 
     return product;
-}
+};
 
 const getMostSoldProductsByDate = async (date: Date) => {
     const orderProducts = await getOrderProductsByDate(date);
 
-    const productIdSoldCountMap = new Map(orderProducts.map(i => [i.productId, orderProducts.filter(filterProduct => filterProduct.productId === i.productId).length]));
-    const getProductsIdsOfSortMap = [...productIdSoldCountMap.entries()].sort((a, b) => b[1] - a[1]).map(keyValue => keyValue[0]);
+    // create Map with product id and product sold count
+    const productIdSoldCountMap = new Map(
+        orderProducts.map((orderProduct) => [
+            orderProduct.productId,
+            orderProducts.filter(
+                (filterProduct) => filterProduct.productId === orderProduct.productId
+            ).length,
+        ])
+    );
 
-    const sliceProductsIds = getProductsIdsOfSortMap.slice(0, CONFIG.MAX_BESTSELLS_PRODUCTS);
+    // get product id array of sorted map by product sold count desc
+    const getProductsIdsOfSortMap = [...productIdSoldCountMap.entries()]
+        .sort((a, b) => b[1] - a[1])
+        .map((keyValue) => keyValue[0]);
+
+    const sliceProductsIds = getProductsIdsOfSortMap.slice(
+        0,
+        CONFIG.MAX_BESTSELLS_PRODUCTS
+    );
 
     const products = await getProductsByIds(sliceProductsIds as number[]);
 
     return products;
-}
+};
 
 export const getAllBestsellerProducts = async () => {
     const bestsellerMonthsPriorToToday = new Date();
-    bestsellerMonthsPriorToToday.setMonth(bestsellerMonthsPriorToToday.getMonth() - CONFIG.LAST_MONTHS_PRODUCTS_IN_ORDER);
+    bestsellerMonthsPriorToToday.setMonth(
+        bestsellerMonthsPriorToToday.getMonth() -
+        CONFIG.LAST_MONTHS_PRODUCTS_IN_ORDER
+    );
 
     return getMostSoldProductsByDate(bestsellerMonthsPriorToToday);
-}
+};
