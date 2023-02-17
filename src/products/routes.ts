d131fc39a4e180f5b643c82666ccedfd9d883f4d
Radseq <radek.gom@gmail.com>
2023-02-17 11:38:04 +0100

    fix: remove zod from validation string to number

--- src/products/routes.ts
+++ src/products/routes.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import { z } from "zod";
 import { getAllProducts, getProductById } from "./product";
 
 export function createProductRouter() {
@@ -10,20 +9,19 @@ export function createProductRouter() {
 			resp.status(200).json(products);
 		})
 		.get("/:productId", async (req: Request, resp: Response) => {
-			const result = z.coerce
-				.number()
-				.min(0)
-				.safeParse(req.params.productId);
+			const productID = parseInt(req.params.productId);
 
-			if (!result.success) {
-				return resp.status(404).send(result.error);
+			if (isNaN(productID)) {
+				return resp
+					.status(404)
+					.send("parm product id, required number got nan/string");
 			}
 
-			const product = await getProductById(result.data);
+			const product = await getProductById(productID);
 
 			if (!product) {
 				return resp.status(404).send({
-					error: `Product ${req.params.productId} not found`,
+					error: `Product with id: ${req.params.productId} not found`,
 				});
 			}
 			return resp.status(200).json(product);
