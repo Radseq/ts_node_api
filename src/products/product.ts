35119d587b50b7feee1c49a70808c64c075919b9
Radseq <radek.gom@gmail.com>
2023-02-19 10:22:29 +0100

    fix: use ts record instead self defined key value pair
    format document

--- src/products/product.ts
+++ src/products/product.ts
@@ -7,54 +7,76 @@ import { getDescriptionsByProductId } from "../descriptions/description";
 import { ProductScore, Specification } from "@prisma/client";
 
 export const getAllProducts = async () => {
-    const allProducts = await prisma.product.findMany();
+	const allProducts = await prisma.product.findMany();
 
-    return allProducts;
-}
+	return allProducts;
+};
 
-export const getProductKeyValueVotes = (productId: number, productScoresDb: ProductScore[]) => {
-    const votes: { [key: number]: number } = {};
-    for (let scoreIndex = 0; scoreIndex < CONFIG.SCORES_MAX_SIZE; scoreIndex++) {
-        votes[scoreIndex] = sumProductVotesByScore(productId, scoreIndex, productScoresDb)
-    }
-    return votes;
-}
+export const getProductKeyValueVotes = (
+	productId: number,
+	productScoresDb: ProductScore[]
+) => {
+	const votes: Record<number, number> = {};
+	for (
+		let scoreIndex = 0;
+		scoreIndex < CONFIG.SCORES_MAX_SIZE;
+		scoreIndex++
+	) {
+		votes[scoreIndex] = sumProductVotesByScore(
+			productId,
+			scoreIndex,
+			productScoresDb
+		);
+	}
+	return votes;
+};
 
-const getSpecificationRecords = (specifications: Specification[], mainType: boolean = false) => {
-    const records: { [key: string]: string } = {};
-    for (let index = 0; index < specifications.length; index++) {
-        if (mainType === specifications[index].isMain) {
-            records[specifications[index].name] = specifications[index].value;
-        }
-    }
-    return records;
-}
+const getSpecificationRecords = (
+	specifications: Specification[],
+	mainType: boolean = false
+) => {
+	const records: Record<string, string> = {};
+	for (let index = 0; index < specifications.length; index++) {
+		if (mainType === specifications[index].isMain) {
+			records[specifications[index].name] = specifications[index].value;
+		}
+	}
+	return records;
+};
 
 export const getProductById = async (productId: number) => {
-    const foundProductById = await prisma.product.findUnique({
-        where: { id: productId }
-    });
+	const foundProductById = await prisma.product.findUnique({
+		where: { id: productId },
+	});
 
-    if (!foundProductById) return null;
+	if (!foundProductById) return null;
 
-    const commentPageIndex = 1;
+	const commentPageIndex = 1;
 
-    const [productScoresDb, productCommentsDb, productSpecificationsDb, productDescriptionDb]
-        = await Promise.all(
-            [getProductScores(productId),
-            getProductComments(foundProductById.id, commentPageIndex, CONFIG.BASE_PRODUCT_COMMENTS_COUNT),
-            getSpecificationsByProductId(productId),
-            getDescriptionsByProductId(productId)]
-        );
+	const [
+		productScoresDb,
+		productCommentsDb,
+		productSpecificationsDb,
+		productDescriptionDb,
+	] = await Promise.all([
+		getProductScores(productId),
+		getProductComments(
+			foundProductById.id,
+			commentPageIndex,
+			CONFIG.BASE_PRODUCT_COMMENTS_COUNT
+		),
+		getSpecificationsByProductId(productId),
+		getDescriptionsByProductId(productId),
+	]);
 
-    return {
-        product: foundProductById,
-        specifications: {
-            main: getSpecificationRecords(productSpecificationsDb, true),
-            other: getSpecificationRecords(productSpecificationsDb)
-        },
-        descriptions: productDescriptionDb,
-        comments: productCommentsDb,
-        scores: getProductKeyValueVotes(productId, productScoresDb)
-    }
-}
+	return {
+		product: foundProductById,
+		specifications: {
+			main: getSpecificationRecords(productSpecificationsDb, true),
+			other: getSpecificationRecords(productSpecificationsDb),
+		},
+		descriptions: productDescriptionDb,
+		comments: productCommentsDb,
+		scores: getProductKeyValueVotes(productId, productScoresDb),
+	};
+};
