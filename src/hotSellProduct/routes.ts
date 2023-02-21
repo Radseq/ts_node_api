b024629ffbdc0ae515662851455275da35c258d3
Radseq <radek.gom@gmail.com>
2023-02-21 12:12:03 +0100

    feat: add cache

--- src/hotSellProduct/routes.ts
+++ src/hotSellProduct/routes.ts
@@ -1,18 +1,24 @@
 import express, { Request, Response } from "express";
 import { getHotSellProduct } from "./hotSellProduct";
+import { getCacheData, setCacheData } from "../cache";
 
 function createHotSellProductRouter() {
-    return express.Router()
-        .get('', async (_, resp: Response) => {
-            const product = await getHotSellProduct();
-            if (!product) {
-                return resp.status(404).send({
-                    error: `No hotSell product found`,
-                });
-            }
-            return resp.status(200).json(product);
-        })
+	return express.Router().get("", async (_, resp: Response) => {
+		let dataResult = await getCacheData("hotsellProduct");
+		if (dataResult) {
+			dataResult = JSON.parse(dataResult);
+		} else {
+			dataResult = await getHotSellProduct();
+			await setCacheData("hotsellProduct", JSON.stringify(dataResult));
+		}
+
+		if (!dataResult) {
+			return resp.status(404).send({
+				error: `No hotSell product found`,
+			});
+		}
+		return resp.status(200).send(dataResult);
+	});
 }
 
 export const hotSellProductRouter = createHotSellProductRouter();
-
